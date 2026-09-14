5887fde9f3c82782413b3cb64e13ec7366edaec9
Атавин Вячеслав ТКИ-142 <optimusslava2008@mail.ru>
2026-09-14 23:48:12 +0300

    Task2.h

---
+++ include/Task2.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Exercise.h"
+
+namespace miit::algebra
+{
+    /**
+     * @brief Задание 2: вставить после каждой четной строки первую строку.
+     */
+    class Task2 : public Exercise
+    {
+    public:
+        /**
+         * @brief Конструктор.
+         * @param matrix Указатель на матрицу.
+         */
+        Task2(Matrix<int>* matrix);
+
+        /**
+         * @brief Выполнить задание.
+         */
+        void solve() override;
+    };
+}
